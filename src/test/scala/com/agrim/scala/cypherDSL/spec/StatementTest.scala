c62ae9a9af89ed6edac36ebdd96157b1c36d4451
Manish Katoch <manish.katoch@gmail.com>
2019-05-08 08:59:10 +0530

    corrects statement tests.

--- src/test/scala/com/agrim/scala/cypherDSL/spec/StatementTest.scala
+++ src/test/scala/com/agrim/scala/cypherDSL/spec/StatementTest.scala
@@ -13,11 +13,11 @@ class StatementTest extends WordSpec with Matchers {
     context.add(person)
 
     "return RETURN string if passed a return clause" in {
-      Statement().toQuery shouldBe ""
+      Statement(Returns(person)).toQuery shouldBe "RETURN a0"
     }
 
     "return empty string if passed no clauses" in {
-      Statement(Returns(person)).toQuery shouldBe "RETURN a0"
+      Statement().toQuery shouldBe ""
     }
   }
 
