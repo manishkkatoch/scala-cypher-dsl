5cd4ba57c6e83a41c48dbb6d23df2b560eacf19d
Swapnil Khandekar <swa.khandekar@gmail.com>
2019-05-08 23:00:34 +0530

    Create Relationship names as snake case

--- src/main/scala/com/agrim/scala/cypherDSL/spec/Node.scala
+++ src/main/scala/com/agrim/scala/cypherDSL/spec/Node.scala
@@ -9,6 +9,8 @@ private[spec] sealed abstract class CypherEntity[T <: Product: QueryProvider, H 
     i0: ToTraversable.Aux[H, List, Symbol]) {
   private val queryProvider = implicitly[QueryProvider[T]]
 
+  def label: String = element.getClass.getSimpleName
+
   def toQuery: String =
     context
       .map(element)(getIdentifierOnlyQuery)
@@ -28,7 +30,7 @@ private[spec] sealed abstract class CypherEntity[T <: Product: QueryProvider, H 
   private def getIdentifierOnlyQuery(id: String): String = makeQuery(id)
 
   private def makeExpandedQuery(id: String, parts: Seq[String]) = {
-    val repr = s"$id:${element.getClass.getSimpleName} {${parts.mkString(",")}}"
+    val repr = s"$id:$label {${parts.mkString(",")}}"
     makeQuery(repr)
   }
 
@@ -41,9 +43,23 @@ private[cypherDSL] case class Node[T <: Product: QueryProvider, H <: HList](elem
     extends CypherEntity(element, properties) {
   override def toQuery: String = s"(${super.toQuery})"
 }
-private[cypherDSL] case class Relationship[T <: Product: QueryProvider, H <: HList](element: T, properties: H)(
+private[cypherDSL] case class Relationship[T <: Product: QueryProvider, H <: HList](element: T, private val properties: H)(
     implicit context: Context,
     i0: ToTraversable.Aux[H, List, Symbol])
     extends CypherEntity(element, properties) {
+
+  private def toUpperSnakeCase(label: String): String = {
+    label
+      .flatMap(char => {
+        if (char.isUpper) Seq("_", char)
+        else Seq(char)
+      })
+      .mkString
+      .stripPrefix("_")
+      .toUpperCase
+  }
+
   override def toQuery: String = s"[${super.toQuery}]"
+
+  override def label: String = toUpperSnakeCase(super.label)
 }
