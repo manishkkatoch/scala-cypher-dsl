7ca1637c05ab8bf5502a27a04332b6f42fa5e79b
Manish Katoch <manish.katoch@gmail.com>
2019-06-07 18:07:23 +0530

    fixing install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,3 +1,3 @@
  #!/usr/bin/env sh
 
- openssl aes-256-cbc -K $encrypted_54b5438000ae_key -iv $encrypted_54b5438000ae_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
+openssl aes-256-cbc -K $encrypted_54b5438000ae_key -iv $encrypted_54b5438000ae_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
