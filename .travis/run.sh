87e8e6085db1a0f55d3dbe054515d93a2e9b19cf
Manish Katoch <manish.katoch@gmail.com>
2019-06-07 18:39:11 +0530

    fixing run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
 echo "running for branch $TRAVIS_BRANCH"
 
-if [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "develop"];
-then
+if [[ "$TRAVIS_BRANCH" = "master" ]] || [[ "$TRAVIS_BRANCH" = "develop"]]; then
     echo "running checks"
 	./gradlew clean check
 else
     echo "Sign, Upload archives to local repo, Upload archives to Sonatype, Close and release repository."
-   	./gradlew uploadArchives publishToNexusAndClose
-
+   	./gradlew uploadArchives
 fi
