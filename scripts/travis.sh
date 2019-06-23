e92cc71117cdf87640f6fdd7cdd5a817a2d89b2c
Manish Katoch <manish.katoch@gmail.com>
2019-06-23 20:28:26 +0530

    moving steps to travis sh.

---
+++ scripts/travis.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+echo "running checks..."
+./gradlew check
+
+if [[ -z $TRAVIS_TAG ]]; then
+    echo "build is not for a tag, that's it then."
+else
+    VERSION=$($TRAVIS_TAG | cut -d 'v' -f 2)
+    echo "the build version will be $VERSION"
+    echo "uploading archives..."
+    BUILD_VERSION=$VERSION ./gradlew uploadArchives
+    echo "upload done."
+fi
