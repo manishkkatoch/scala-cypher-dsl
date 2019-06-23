3db276ca500ea95dd73201fdc43883ed1ab46842
Manish Katoch <manish.katoch@gmail.com>
2019-06-23 21:05:32 +0530

    fixes travis scripts

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,7 +6,7 @@ echo "running checks..."
 if [[ -z $TRAVIS_TAG ]]; then
     echo "build is not for a tag, that's it then."
 else
-    VERSION=$($TRAVIS_TAG | cut -d 'v' -f 2)
+    VERSION=$(echo $TRAVIS_TAG | cut -d 'v' -f 2)
     echo "the build version will be $VERSION"
     echo "uploading archives..."
     BUILD_VERSION=$VERSION ./gradlew uploadArchives
